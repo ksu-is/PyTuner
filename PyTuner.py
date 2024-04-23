9675d1b3a228a7f51e234b0c29416b94daa1459e
lreinha1 <lreinha1@students.kennesaw.edu>
2024-04-22 22:38:24 -0400

    Re-insert comments
    Accidentally deleted some of the comments. Re-inserted them

--- PyTuner.py
+++ PyTuner.py
@@ -43,7 +43,7 @@ NOTE_NAMES = 'C C# D D# E F F# G G# A A# B'.split()
 
 def freq_to_number(f): return 69 + 12*np.log2(f/440.0)
 def number_to_freq(n): return 440 * 2.0**((n-69)/12.0)
-def note_name(n): return NOTE_NAMES[n % 12] + str(n/12 - 1)
+def note_name(n): return NOTE_NAMES[n % 12] + str(n//12 - 1)
 
 ######################################################################
 # Ok, ready to go now.
